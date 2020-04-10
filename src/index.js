02640949f8980e1bc045c4706fd3adec27a00b26
Gyumong <rlaalsrb3559@naver.com>
2020-04-10 16:47:47 +0900

    redux-saga 써보기

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 // import loggerMiddleware from './lib/loggerMiddleware';
 import {createLogger} from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 const logger = createLogger();
-const store =createStore(rootReducer , applyMiddleware(logger,ReduxThunk));
+const SagaMiddleware = createSagaMiddleware();
+const store =createStore(rootReducer , composeWithDevTools(applyMiddleware(logger,ReduxThunk, SagaMiddleware)));
+SagaMiddleware.run(rootSaga);
 ReactDOM.render(
   <Provider store={store}>
     <App />
